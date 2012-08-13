c9a06fbb1b55a04cd58ce30fa7a294dd5bb6e269
Fabrício Godoy <skarllot@gmail.com>
2012-08-12 23:48:15 -0300

    Correção no view "historico_yield_anual"

--- schema/views/historico_yield_anual.sql
+++ schema/views/historico_yield_anual.sql
@@ -3,7 +3,7 @@ CREATE VIEW `historico_yield_anual` AS
 SELECT cod_ativo,
     YEAR(data_base) as ano,
     COUNT(*) as qtd,
-    CAST(EXP(SUM(LOG(perc_rend+1)))-1 as DECIMAL(16,5)) as yield,
-    CAST(STDDEV_SAMP(perc_rend)*SQRT(count(*)) as DECIMAL(16,5)) as volatilidade
+    CAST(EXP(SUM(LOG(yield+1)))-1 as DECIMAL(16,5)) as yield,
+    CAST(STDDEV_SAMP(yield)*SQRT(count(*)) as DECIMAL(16,5)) as volatilidade
 FROM historico_yield
 GROUP BY cod_ativo, YEAR(data_base)
